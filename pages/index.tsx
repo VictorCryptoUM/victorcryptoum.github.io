f8add8ced543b56dc2f543b4658d4d7af018a9ef
jbis9051 <josh9051@gmail.com>
2022-02-20 02:22:38 -0500

    Typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,13 +16,13 @@ const Home: NextPage = () => {
                 <div className={styles.content}>
                     <h1 className={styles.title}>Go Blue Crypto</h1>
                     <p className={styles.info}>
-                        Go Blue Crypto is cryptography club at the University of
-                        Michigan. We host meetings, round table discussions, and
-                        more on both primitive and applied cryptography as well
-                        as privacy topics. The club is composed of students,
-                        alumni, and staff who are focused on learning and using
-                        cryptography. Morals and ethics are fundamental part of
-                        cryptography and what we do.
+                        Go Blue Crypto is the cryptography club at the
+                        University of Michigan. We host meetings, round table
+                        discussions, and more on both primitive and applied
+                        cryptography as well as privacy topics. The club is
+                        composed of students, alumni, and staff who are focused
+                        on learning and using cryptography. Morals and ethics
+                        are fundamental part of cryptography and what we do.
                     </p>
                     <div className={styles.buttons}>
                         <Button className={styles.button}>About</Button>
